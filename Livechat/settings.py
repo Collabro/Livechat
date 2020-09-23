d69389870f92b018ba836e53cd0e08b415675bb4
AnuragGupta806 <54318429+AnuragGupta806@users.noreply.github.com>
2020-09-24 01:11:10 +0530

    Chatbox added

--- Livechat/settings.py
+++ Livechat/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -69,6 +70,17 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'Livechat.wsgi.application'
+
+ASGI_APPLICATION = "Livechat.routing.application"
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 
 # Database
--- chatbox/views.py
+++ chatbox/views.py
@@ -1,6 +1,20 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+import json
+from django.utils.safestring import mark_safe
 
 # Create your views here.
 
-def index(request):
+def home(request):
     return render(request, 'chatbox/home.html')
+
+
+def index(request):
+    return render(request, 'chatbox/index.html')
+
+@login_required
+def room(request, room_name):
+    return render(request, 'chatbox/room.html', {
+        'room_name_json': mark_safe(json.dumps(room_name)),
+        'username': mark_safe(json.dumps(request.user.username)),
+    })
